1fcb6b6c8fa9ecb58cc0845af54f58cab5c5f53d
Solomon Antony <santony@murraystate.edu>
2015-10-08 20:22:14 +0000

    Finished static pages app

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | Solomon's sample app"
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "Help | Solomon's sample app"
+  end
+ 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Solomon's sample app"
+  end
+
+  
+ 
+end
